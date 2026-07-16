1748e4a6e1828fce64090ddf2c41cd39dd607716
Maksim Petyakin <maks.petyakin@yandex.ru>
2026-07-16 13:23:52 +0300

    finish

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(s string) (string, error) {
+	s = strings.TrimSpace(s)
+
+	if s == "" {
+		return "", errors.New("empty string")
+	}
+
+	if strings.Trim(s, ".- ") == "" {
+		return morse.ToText(s), nil
+	}
+
+	return morse.ToMorse(s), nil
+}
